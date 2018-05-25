1f10a63a4740a317eb5fe45b6326d1cdac028007
Taylor Rauch <Taylor@students-mbp-4.lan>
2018-05-25 13:40:27 -0400

    props on route

--- counterpoints/src/Route.js
+++ counterpoints/src/Route.js
@@ -5,12 +5,32 @@ import Homepage from "./homepage.js";
 import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 
 export default class Route extends Component {
+  constructor() {
+    this.state = {
+      topic: "",
+      option: 1
+    };
+  }
+  updateField(field, newValue) {
+    this.setState({
+      [field]: newValue
+    });
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div>
           <Redirect to="/Main" />
-          <Route path="/Main" component={Homepage} />
+          <Route
+            path="/Main"
+            render={() => (
+              <Homepage
+                updateTopic={newVal => this.updateField("topic", newVal)}
+                updateOption={newVal => this.updateField("option", newVal)}
+              />
+            )}
+          />
           <Route path="/Results" component={App} />
         </div>
       </BrowserRouter>
